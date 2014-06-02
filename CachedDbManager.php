6f1000a6f701d25665c52583d76533a81ee1e1e5
bethrezen <b37hr3z3n@gmail.com>
2014-06-02 12:36:46 +0400

    First implementation

---
+++ CachedDbManager.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @link https://github.com/bethrezen/yii2-cached-rbac-dbmanager
+ */
+namespace bethrezen;
+
+/**
+ * CachedDbManager allows standard yii2 DbManager rbac component caching of permission requests
+ *
+ * @author Alexander Kozhevnikov <b37hr3z3n@gmail.com>
+ */
+class CachedDbManager extends \yii\rbac\DbManager
+{
+    /**
+     * @var \yii\caching\Cache Application cache component that will be used for caching
+     */
+    public $cache = 'cache';
+
+    /**
+     * @var integer Lifetime of cached data in seconds
+     */
+    public $lifetime = 3600;
+
+    public $cachePrefix = 'CachedDbManager_';
+
+    public function checkAccess($userId, $permissionName, $params = [])
+    {
+        
+        if (count($params)>0) {
+            return parent::checkAccess($userId, $permissionName, $params);
+        }
+
+        $cacheKey = $this->cachePrefix.'userAccessCheck:'.$userId.':'.$permissionName;
+
+        /*
+            Due to yii2 cache system, where we receive 'false' from cache component
+            we have to store array in cache to ensure that 'false' doesn't mean
+            that access is restricted
+        */
+        $check = $this->getCache()->get($cacheKey);
+
+        if (!is_array($check)) {
+            $check = [
+                parent::checkAccess($userId, $permissionName, $params)
+            ];
+
+            Yii::$app->cache->set($cacheKey, $check, $this->lifetime);
+        }
+
+        return $check[0];
+    }
+    
+    private function getCache()
+    {
+        return Yii::$app->get($this->cache);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-yii2-cached-rbac-dbmanager
-==========================
-
 Cached DBManager for yii2 RBAC
+==============================
+Allows caching of permissions in yii\rbac\DbManager 
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist bethrezen/yii2-cached-rbac-dbmanager "*"
+```
+
+or add
+
+```
+"bethrezen/yii2-cached-rbac-dbmanager": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply configure your application to use `\bethrezen\CachedDbManager`:
+
+```
+
+'components' => [
+    // the rest of your components section
+    'authManager' => [
+            'class'=>'\bethrezen\CachedDbManager',
+        ],
+]
+
+```
